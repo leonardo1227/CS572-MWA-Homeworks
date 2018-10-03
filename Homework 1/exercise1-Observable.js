65457766002b6120f79310c8642f7281b1627686
Leonardo Samuel Tolosa Contreras <leonardo1227@hotmail.com>
2018-10-03 16:41:58 -0500

    commit
    commit

--- Homework 1/exercise1-Observable.js
+++ Homework 1/exercise1-Observable.js
@@ -1,15 +1,33 @@
 //Student: Leonardo Samuel Tolosa Contreras
 //Student ID: 986527
 //Course: CS572-Modern Web Applications (MUM)
-//Assignment 1
+//Assignment 1 (Observable)
 
-Array.prototype.removeDuplicates = function(){
-    let numbers = this;
-    let promise = new Promise((resolve) => {
-        resolve(numbers.filter((number,position,array) => array.indexOf(number)==position));
+const {Observable} = require("rxjs");
+
+function filterNumbersPromise(numbers){
+    return new Promise((resolve,reject) => {
+        if(numbers.length>0){
+            resolve(Array.from(new Set(numbers)));
+        }else{
+            reject('!!!Empty Array!!!');
+        }
     });
-    return promise;
 };
 
-[4,1,5,7,2,3,1,4,6,5,2].removeDuplicates()
-            .subscribe(s=>s.then(result => console.log(result)));
+Array.prototype.removeDuplicates = function(){
+    return new Observable(observer => {
+        setTimeout(()=>{
+            observer.next(filterNumbersPromise(this))
+        },1000);
+    })
+};
+
+console.log('--> START');
+console.log('Input:')
+console.log([4,1,5,7,2,3,1,4,6,5,2]);
+let subscription = [4,1,5,7,2,3,1,4,6,5,2].removeDuplicates();
+subscription.subscribe(result => {
+    console.log('Output:')
+    console.log(result)});
+
