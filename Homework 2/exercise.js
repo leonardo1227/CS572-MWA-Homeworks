c4592bd8bccfb3024ccf40ce2d6ca10c2c989b52
Leonardo Samuel Tolosa Contreras <leonardo1227@hotmail.com>
2018-10-04 01:08:55 -0500

    commit
    commit

--- Homework 2/exercise.js
+++ Homework 2/exercise.js
@@ -3,21 +3,30 @@
 //Course: CS572-Modern Web Applications (MUM)
 //Assignment 2
 
+async function processNumbers(x,numbers){
+    try{
+        return await numbers.filter(n=>n%2==x);
+    }catch(error){
+        return error;
+    }
+}
 
-function processNumbers(x,numbers){
-    return numbers.filter(n=>n%2==x);
+function executesProcessNumbersAsync(x,numbers){
+    processNumbers(x,numbers)    
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
 }
 
 Array.prototype.even = function(){
-    console.log(processNumbers(0,this));
+    executesProcessNumbersAsync(0,this);
 }
 
 Array.prototype.odd = function(){
-    console.log(processNumbers(1,this));
+    executesProcessNumbersAsync(1,this);
 }
 
 console.log('start');
-setImmediate(()=>{[1,2,3,4,5,6,7,8].even();});
-setImmediate(()=>{[1,2,3,4,5,6,7,8].odd();});
+[1,2,3,4,5,6,7,8].even();
+[1,2,3,4,5,6,7,8].odd();
 console.log('end');
 
