196c49fb8b56692211c6be7811e0c90629e3d205
Leonardo Samuel Tolosa Contreras <leonardo1227@hotmail.com>
2018-10-09 00:50:20 -0500

    Assignment 7
    Assignment 7

--- Homework 7/exercise2.js
+++ Homework 7/exercise2.js
@@ -2,4 +2,53 @@
 //Student ID: 986527
 //Course: CS572-Modern Web Applications (MUM)
 //Assignment 7
+const express = require('express');
+const mongoClient = require('mongodb').MongoClient;
+const crypto = require('crypto');
+const {Subject} = require('rxjs');
 
+const app = express();
+app.set('port',1000);
+
+const dbInfo = {url:'mongodb://127.0.0.1:27017/local',
+                dbName:'myDB',
+                configurations:{useNewUrlParser:true}};
+const cryptoInfo = {algorithm:'aes256',
+                    password:'asaadsaad'};
+
+const decipher = crypto.createDecipher(cryptoInfo.algorithm,cryptoInfo.password);
+
+const dbDataRetriver = new Subject();
+const decrypter = new Subject();
+const responseWriter = new Subject();
+
+app.get('/secret',(request,response)=>{
+    dbDataRetriver.next({request:request,
+                    response:response});
+});
+
+dbDataRetriver.subscribe(data =>{
+    mongoClient.connect(dbInfo.url, dbInfo.configurations, (err,client)=>{
+        if(err) throw err;
+        let dbconnection = client.db(dbInfo.dbName);
+        dbconnection.collection('homework7').findOne({},{projection:{_id:0}},(err,doc)=>{
+            if(err) throw err;
+            data.message=doc.message;
+            decrypter.next(data);
+            client.close();
+        });
+    });
+});
+
+decrypter.subscribe(data =>{
+    data.decryptedMessage = decipher.update(data.message,'hex','utf8');
+    data.decryptedMessage += decipher.final('utf8');
+    responseWriter.next(data);
+});
+
+responseWriter.subscribe(data =>{
+    data.response.json({message:data.decryptedMessage});
+});
+
+
+app.listen(app.get('port'),()=>console.log(`Server started and is running to port ${app.get('port')}`));
