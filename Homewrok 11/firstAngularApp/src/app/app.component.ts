ccda1f76b0f6cd8589540111072fedc2f5fdccc6
Leonardo Samuel Tolosa Contreras <leonardo1227@hotmail.com>
2018-10-16 09:58:29 -0500

    Assingment 11
    Assignment 11

--- Homewrok 11/firstAngularApp/src/app/app.component.ts
+++ Homewrok 11/firstAngularApp/src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<counter #counterS [counter]="10" (counterChange)="updateComponentCounterValue($event)"></counter> <br>
-              <output>Component Counter Value = {{componentCounterValue}}</output>`,
-  //styleUrls: ['./app.component.css']
+  template: `<counter #counterS [counter]="componentCounterValue" (counterChange)="updateComponentCounterValue($event)"></counter> <br>
+              <output>Component Counter Value = {{componentCounterValue}}</output>`
 })
 export class AppComponent {
   title = 'firstAngularApp';
-  componentCounterValue:number;
+  componentCounterValue:number=10;
 
   updateComponentCounterValue(externalValue){
     this.componentCounterValue=externalValue;
